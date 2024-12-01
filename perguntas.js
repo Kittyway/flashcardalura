b7b7939fdf0441fe44bf9932a372b94190cca52b
Kittyway <166880687+Kittyway@users.noreply.github.com>
2024-12-01 10:39:52 -0300

    trabalho completo.

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação', 
+    'O  que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é uma função',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Lingua Inglesa',
+    'Como se diz oi em inglês',
+    'oi  em inglês e HI LORENA (RAI)'
+)
+
+criaCartao (
+    'Academia',
+    'Qual o melhor dia para treinar perna?',
+    'Segunda-Feira ou Quinta-Feira.'
+)
+
+criaCartao (
+    'Programação',
+    'O que é CSS',
+    'CSS é uma linguagem de estilização'
+)
+
+criaCartao (
+    'Astrologia',
+    'Quantas Luas tem Marte e quais são os seus nomes.',
+    'Marte tem dois, chamados de Fobos e Deimos'
+)
+
+criaCartao (
+    'Programação',
+    'Por quais linguagens eu poderia começar a programar',
+    'Uma das melhores linguagens para começar na programaçao é HTML e CSS'
+)
+
+criaCartao (
+    'Programação',
+    'Como eu poderia começar programação de graça e com certificado?',
+    'Online poderia fazer o "CURSOEMVIDEO " do Gustavo Guanabara. Presencial, poderia fazer na instituição da FIEB em Barueri.'
+)
